4a01245e90016d65d77bdaf55804b2f049249e90
unknown <artemvakulenko42@gmail.com>
2021-05-04 23:57:19 +0300

    minor improvements in VisaApp

--- VisaToOmerikaApp/src/index.js
+++ VisaToOmerikaApp/src/index.js
@@ -1,9 +1,10 @@
-var nameArray = ['вася', 'петя', 'вася', 'петя','вася', 'петя','вася', 'петя','вася', 'петя',]
-var balanceArray = [7000, 7000, 7000, 7000, 7000, 7000, 2000, 2000, 2000, 2000]
+var nameArray = ['1', '2', '3', '4','5', '6', '7', '8', '9', '10',]
+var balanceArray = [7000, 7000, 7000, 7000, 7000, 7000, 1000, 1000, 1000, 1000]
 var ageArray = [20, 20, 20, 20, 20, 20, 20, 70, 70, 70]
 var docArray = [true, true, true, true, true, true, true, true, false, false]
 var engArray = [true, true, true, false, false, false, false, false, false, false]
 
+var errorText = document.getElementById('error')
 var nameInput = document.getElementById('name')
 var generateNameBtn = document.getElementById('generateName')
 
@@ -24,6 +25,8 @@ var addCandidateButton =  document.getElementById('addCandidateButton')
 var initButton =  document.getElementById('initButton')
 var raceButton =  document.getElementById('raceButton')
 var clearButton =  document.getElementById('clearButton')
+
+var winnerText =  document.getElementById('winner')
 
 function getRandom1to10(){
 	return Math.floor (Math.random() * 10)
@@ -62,17 +65,60 @@ function Candidate(name, balance, age, docs, eng){
     this.eng = eng
 }
 addCandidateButton.addEventListener('click', function(){
-    //сюда нужна ещё функция проверки имени на пустоту
-    var cand = new Candidate(
-        nameInput.value, 
-        balanceInput.value,
-        ageInput.value, 
-        documentInput.value, 
-        engInput.value
-        )
-    candidates.push(cand)
-    console.log(candidates);
-    if(candidates.length === 5){
-        addCandidateButton.disabled = 'true'
+    if(!nameInput.value) {
+        errorText.innerHTML = 'no name'
+    }else{
+        //сюда нужна ещё функция проверки имени на пустоту
+        var cand = new Candidate(
+            nameInput.value, 
+            checkBalance(balanceInput.value),
+            checkAge(ageInput.value), 
+            checkDocs(documentInput.value), 
+            checkEng(engInput.value)
+            )
+        candidates.push(cand)
+        console.log(candidates);
+        if(candidates.length === 5){
+            addCandidateButton.disabled = 'true'
+        }
     }
+})
+function checkBalance(num){
+    if(num > 2000) return true
+    else return false
+}
+function checkAge(num){
+    if(num > 18 && num < 60) return true
+    else return false
+}
+
+//написать проверку введенных документов и добавить в конструктор
+function checkDocs(str){
+    if(str = 'true') return true
+    if(str = 'false') return true
+}
+//написать проверку введенного уровня инглиша и добавить в конструктор
+function checkEng(str){
+    if(str = 'true') return true
+    if(str = 'false') return true
+}
+
+
+initButton.addEventListener('click', function(){
+    //функция показа всех кандидатов
+})
+
+
+raceButton.addEventListener('click', function(){
+    //функция запускает гонку кандидатов
+    startPromiceRace(candidates)
+})
+
+function startPromiceRace(array){
+
+}
+//убрать задизейбливание кнопки add после нажания та clear
+clearButton.addEventListener('click', function(){
+    candidates = []
+    console.log(candidates);
 })
